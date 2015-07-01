12dff3d1aa3e9dd38b7dcb497f267b2e104694e1
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-07-01 13:05:33 -0300

    Renamed the ViewController to ArticlesListViewController. I will use this to list articles to the user.

---
+++ Reader/Reader/ViewController/ArticlesListViewController.h
@@ -0,0 +1,15 @@
+//
+//  ArticlesListViewController.h
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 7/1/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticlesListViewController : UIViewController
+
+
+@end
+
---
+++ Reader/Reader/ViewController/ArticlesListViewController.m
@@ -0,0 +1,27 @@
+//
+//  ArticlesListViewController.m
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 7/1/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ArticlesListViewController.h"
+
+@interface ArticlesListViewController ()
+
+@end
+
+@implementation ArticlesListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
