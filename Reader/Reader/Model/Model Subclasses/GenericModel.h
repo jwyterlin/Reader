4fc1a3958fcd16d306ac4066ed6ae4334ae88a81
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-07-01 13:16:37 -0300

    Created a file GenericModel to abstract some methods that will be used in every model classes

---
+++ Reader/Reader/Model/Model Subclasses/GenericModel.h
@@ -0,0 +1,13 @@
+//
+//  GenericModel.h
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 7/1/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GenericModel : NSObject
+
+@end
---
+++ Reader/Reader/Model/Model Subclasses/GenericModel.m
@@ -0,0 +1,13 @@
+//
+//  GenericModel.m
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 7/1/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericModel.h"
+
+@implementation GenericModel
+
+@end
