ed9002205a9f03171b83a5df47bf0bb5dc55f0eb
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-07-01 13:30:10 -0300

    Created class GenericDAO to import some files that will be used in all DAO classes

---
+++ Reader/Reader/DAO/GenericDAO.h
@@ -0,0 +1,13 @@
+//
+//  GenericDAO.h
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 7/1/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GenericDAO : NSObject
+
+@end
---
+++ Reader/Reader/DAO/GenericDAO.m
@@ -0,0 +1,13 @@
+//
+//  GenericDAO.m
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 7/1/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericDAO.h"
+
+@implementation GenericDAO
+
+@end
