ad45f21abe04bf4c66221e9a3b8238e1cad7acda
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-07-02 00:49:37 -0300

    Created file ArticleDetailViewController to show the content about the article selected

---
+++ Reader/Reader/ViewController/ArticleDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArticleDetailViewController.h
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 02/07/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticleDetailViewController : UIViewController
+
+@end
---
+++ Reader/Reader/ViewController/ArticleDetailViewController.m
@@ -0,0 +1,27 @@
+//
+//  ArticleDetailViewController.m
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 02/07/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ArticleDetailViewController.h"
+
+@interface ArticleDetailViewController()
+
+@end
+
+@implementation ArticleDetailViewController
+
+#pragma mark - View Lifecycle
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
