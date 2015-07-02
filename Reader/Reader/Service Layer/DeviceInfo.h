4f555b160f8dfdd39ecbd135452c7787010f5fdf
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-07-02 03:53:39 -0300

    Changing UILabels to JWLabels to get height dynamic in cells

---
+++ Reader/Reader/Service Layer/DeviceInfo.h
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.h
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 02/07/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceInfo : NSObject
+
+@end
---
+++ Reader/Reader/Service Layer/DeviceInfo.m
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.m
+//  Reader
+//
+//  Created by Jhonathan Wyterlin on 02/07/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "DeviceInfo.h"
+
+@implementation DeviceInfo
+
+@end
